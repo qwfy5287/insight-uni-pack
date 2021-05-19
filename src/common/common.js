819c9ea8a50d342daa544eca8b1b9b4dca47c0f3
qwfy5287 <qwfy5287@qq.com>
2021-05-19 10:43:57 +0800

    chore: prettier 2

--- src/common/common.js
+++ src/common/common.js
@@ -2,6 +2,6 @@
  * 处理异常，输出信息，不中断代码
  * @param {Object} err 异常信息
  */
-export const handleCatch = err => {
+export const handleCatch = (err) => {
   console.error('🚀 ~ file: common.js ~ line 6 ~ handleCatch ~ err', err)
 }
